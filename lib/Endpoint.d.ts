455c20de82b2fd845043a12cfb9c2c39c034cfbf
dimitar <dimitar@doonloo.com>
2018-01-03 17:35:13 +0200

    Remove duplicate declarations

--- lib/Endpoint.d.ts
+++ lib/Endpoint.d.ts
@@ -16,24 +16,3 @@ export declare class Endpoint {
     setCache(...args: any[]): void;
 
 }
-
-export declare namespace Endpoint {
-    namespace prototype {
-        function disableCaching(...args: any[]): void;
-
-        function enableCaching(...args: any[]): void;
-
-        function executingRequest(...args: any[]): void;
-
-        function flushCache(...args: any[]): void;
-
-        function getConfig(...args: any[]): void;
-
-        function isDebugging(...args: any[]): void;
-
-        function setCache(...args: any[]): void;
-
-    }
-
-}
-
